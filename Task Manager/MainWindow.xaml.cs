d9501872b74843268374f3d13f4aea42ee359d72
Ho Wai Yung <howaiyung1988@gmail.com>
2019-12-08 19:21:32 -0700

    Added functionality of adding items into grid box

--- Task Manager/MainWindow.xaml.cs
+++ Task Manager/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,11 +21,38 @@ namespace Task_Manager
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        ObservableCollection<Task> Task_ListBoxItems = new ObservableCollection<Task>();
 
+        private void Add_Task(object sender, RoutedEventArgs e)
+        {
+            
+
+            Task newTask = new Task();
+
+            newTask.Description = taskInfo.Text;
+            newTask.Date = Convert.ToString(dueDate.SelectedDate);
+            newTask.TaskComplete = true;
+
+            Task_ListBoxItems.Add(newTask);
+
+            dgTasks.ItemsSource = Task_ListBoxItems;
+
+
+        }
+
+        public class Task
+        {
+            public string Description { get; set; }
+            public string Date { get; set; }
+            public Boolean TaskComplete { get; set; }
+
+        }
     }
 }
